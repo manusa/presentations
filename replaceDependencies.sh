ef8037b9dc5ee261de950b6e0f95b77b5c77b03d
Marc Nuri <marc@marcnuri.com>
2019-01-04 14:09:17 +0100

    Tweaked travis to include static files

--- replaceDependencies.sh
+++ replaceDependencies.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-cat "${TRAVIS_BUILD_DIR}"/package.json | \
-    jq -r '.dependencies = {"express": "^4.16.14"}' > "${TRAVIS_BUILD_DIR}"/package.json
+mv "${TRAVIS_BUILD_DIR}"/package.json "${TRAVIS_BUILD_DIR}"/tmp-package.json
+cat "${TRAVIS_BUILD_DIR}"/tmp-package.json \
+    | jq -r '.dependencies = {"express": "^4.16.14"}' | cat > "${TRAVIS_BUILD_DIR}"/package.json
+rm "${TRAVIS_BUILD_DIR}"/tmp-package.json
