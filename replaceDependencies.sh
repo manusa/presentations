469ef94a3641d91073be820470549a76c77d434f
Marc Nuri <marc@marcnuri.com>
2019-01-04 14:14:07 +0100

    Updated express version

--- replaceDependencies.sh
+++ replaceDependencies.sh
@@ -2,5 +2,5 @@
 
 mv "${TRAVIS_BUILD_DIR}"/package.json "${TRAVIS_BUILD_DIR}"/tmp-package.json
 cat "${TRAVIS_BUILD_DIR}"/tmp-package.json \
-    | jq -r '.dependencies = {"express": "^4.16.14"}' | cat > "${TRAVIS_BUILD_DIR}"/package.json
+    | jq -r '.dependencies = {"express": "^4.16.4"}' | cat > "${TRAVIS_BUILD_DIR}"/package.json
 rm "${TRAVIS_BUILD_DIR}"/tmp-package.json
