5142b2e2b8b902895e29db81e92ee8131e4e30f4
Marc Nuri <marc@marcnuri.com>
2019-01-04 21:51:10 +0100

    Base styles + landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
+import './index/index.scss';
+import '../styles/main.scss';
 
 const Index = () => (
-    <div>
+    <div className={'main'}>
       <Helmet>
         <meta charSet="UTF-8" />
         <title>Marc Nuri Tech Talks</title>
       </Helmet>
+      <h1>Marc Nuri's Tech Talks</h1>
       <p>Work in progress</p>
     </div>
   );
