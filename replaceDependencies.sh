9b6a5d8713f9863f6259bfb5a9dbab13dc0941bc
Marc Nuri <marc@marcnuri.com>
2021-10-02 07:20:30 +0200

    👷 Replace TravisCI with GitHub Actions

--- replaceDependencies.sh
+++ replaceDependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mv "${TRAVIS_BUILD_DIR}"/package.json "${TRAVIS_BUILD_DIR}"/tmp-package.json
-cat "${TRAVIS_BUILD_DIR}"/tmp-package.json \
-    | jq -r '.dependencies = {"express": "^4.16.4"}' | cat > "${TRAVIS_BUILD_DIR}"/package.json
-rm "${TRAVIS_BUILD_DIR}"/tmp-package.json
+mv package.json tmp-package.json
+cat tmp-package.json \
+    | jq -r '.dependencies = {"express": "^4.17.1"}' | cat > package.json
+rm tmp-package.json
