006a6c2b02991baba782d5243a0f38d473265136
Marc Nuri <marc@marcnuri.com>
2024-06-07 16:00:38 +0200

    ✨ DevBcn 2024 kubctl --watch

--- src/pages/presentations/2024-devbcn-full-stack-reactive-application/slide-070-kubernetes-streams.jsx
+++ src/pages/presentations/2024-devbcn-full-stack-reactive-application/slide-070-kubernetes-streams.jsx
@@ -1,14 +1,66 @@
 import React from 'react';
 import slideControls from '../../../components/slide-controls/slide-controls';
-import {DevBcn2024, kubernetesComponentsDiagram} from '../../../components';
+import {Code, DevBcn2024} from '../../../components';
 
-const Slide070 = () => {
+const CmdWindow = ({style, children}) => {
+  return (
+    <Code language='text' customStyle={{flex: 1, fontSize: '1.5rem', background: '#232323CC', ...style}}>
+        {children}
+    </Code>
+  )
+};
+
+const podsSteps = {
+  2: ['$ kubectl get pods --watch'],
+  3: ['NAME               READY   STATUS              RESTARTS   AGE'],
+  4: ['devbcn-2024-5c65   0/1     Pending             0          0s'],
+  5: ['devbcn-2024-5c65   0/1     ContainerCreating   0          1s'],
+  6: ['devbcn-2024-5c65   0/1     Running             0          4s'],
+  7: ['devbcn-2024-5c65   1/1     Running             0          8s']
+};
+
+
+const deploySteps = {
+  2: ['$ kubectl get deploy --watch'],
+  3: [
+    'NAME          READY   UP-TO-DATE   AVAILABLE   AGE',
+    'devbcn-2024   0/0     0            0           0s'
+  ],
+  4: ['devbcn-2024   0/1     0            0           1s'],
+  5: ['devbcn-2024   0/1     0            0           2s'],
+  6: ['devbcn-2024   0/1     1            0           5s'],
+  7: ['devbcn-2024   1/1     1            1           9s']
+};
+
+const reduceStep = ({currentStep, obj}) => {
+  let ret = '';
+  for (let it = 0; it <= currentStep; it++) {
+    if (obj[it]) {
+      for (const line of obj[it]) {
+        ret += line + '\n';
+      }
+    }
+  }
+  return ret;
+}
+
+const Slide070 = ({currentStep}) => {
   return (
     <DevBcn2024.SlideTemplate slide={7} title='Kubernetes Streams'>
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'stretch'}}>
         <img
-          style={{height: '30rem'}}
-          src={DevBcn2024.kubernetesStreamResources} alt='A diagram of the Kubernetes components'/>
+          style={{alignSelf: 'center', width: '78rem'}}
+          src={DevBcn2024.kubernetesStreamResources}
+          alt='A diagram of the Kubernetes components'
+        />
+        <div style={{display: currentStep > 1 ? 'flex' : 'none', justifyContent: 'center', gap: '2rem'}}>
+          <CmdWindow style={{width: 'auto'}}>{
+            reduceStep({currentStep, obj: podsSteps})
+          }</CmdWindow>
+          <CmdWindow style={{width: 'auto'}}>{
+            reduceStep({currentStep, obj: deploySteps})
+          }</CmdWindow>
+        </div>
       </div>
     </DevBcn2024.SlideTemplate>
   );
@@ -16,4 +68,5 @@ const Slide070 = () => {
 
 export default slideControls(Slide070,
   `/presentations/${DevBcn2024.SLUG}/slide-060-kubernetes-event-producer`,
-  `/presentations/${DevBcn2024.SLUG}/slide-080-yakd-case-study`);
+  `/presentations/${DevBcn2024.SLUG}/slide-080-yakd-case-study`,
+  7);
