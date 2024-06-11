3593844c2887adb7ad367fb4e5d7327308754f88
Marc Nuri <marc@marcnuri.com>
2024-06-11 15:23:11 +0200

    ✨ DevBcn 2024 agenda, goal of the presentation

--- src/pages/presentations/2024-devbcn-full-stack-reactive-application/slide-020-agenda.jsx
+++ src/pages/presentations/2024-devbcn-full-stack-reactive-application/slide-020-agenda.jsx
@@ -3,30 +3,40 @@ import slideControls from '../../../components/slide-controls/slide-controls';
 import {DevBcn2024} from '../../../components';
 
 const Slide020 = () => {
-  // TODO:
-  //  - What is the goal of the presentation?
-  //    -
   return (
     <DevBcn2024.SlideTemplate slide={2} title='Agenda'>
-      <div style={{height: '100%', display: 'flex', gap: '0rem', alignItems: 'center'}}>
-        <DevBcn2024.DevBcn
-          style={{width: '30rem', fill: 'white', transform: 'rotate(270deg)'}}
-          alt='An image of the DevBcn logo'
-        />
-        <ul style={{flex: 1}}>
-          <li>What is a Reactive Application?</li>
-          <li>Kubernetes as an Event Producer</li>
-          <li>
-            Case study: The YAKD project
-            <ul>
-              <li>Building a resilient Backend with Quarkus</li>
-              <li>Frontend: Consuming Events Reactively</li>
-            </ul>
-          </li>
-          <li>Advantages of Reactive Applications</li>
-          <li>Tradeoffs and Challenges</li>
-          <li>Q&A</li>
-        </ul>
+      <div style={{height: '100%', display: 'flex', flexDirection: 'column'}}>
+        <div style={{
+          marginTop: '5rem',
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          textAlign: 'center',
+          fontFamily: '\'Lionello\', sans-serif',
+          fontSize: '5rem',
+          textShadow: '0rem -2rem 0.9rem #FFFFFF66'
+        }}>
+          How to consume events reactively?
+        </div>
+        <div style={{flex: 1, display: 'flex', gap: '0rem', alignItems: 'center'}}>
+          <DevBcn2024.DevBcn
+            style={{width: '30rem', fill: 'white', transform: 'rotate(270deg)'}}
+            alt='An image of the DevBcn logo'
+          />
+          <ul style={{flex: 1}}>
+            <li>What is a Reactive Application?</li>
+            <li>Kubernetes as an Event Producer</li>
+            <li>
+              Case study: Yet Another Kubernetes Dashboard (YAKD)
+              <ul>
+                <li>Building a resilient Backend with Quarkus</li>
+                <li>Frontend: Consuming Events Reactively</li>
+              </ul>
+            </li>
+            <li>Advantages of Reactive Applications</li>
+            <li>Tradeoffs and Challenges</li>
+            <li>Q&A</li>
+          </ul>
+        </div>
+
       </div>
     </DevBcn2024.SlideTemplate>
   );
