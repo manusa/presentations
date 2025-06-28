6b2edc26009439c7572f103dc83347d6f39fa162
Marc Nuri <marc@marcnuri.com>
2025-06-28 16:49:56 +0200

    ♻️ slide controls uses functional component

--- src/components/slide-controls/slide-controls.jsx
+++ src/components/slide-controls/slide-controls.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {navigate} from 'gatsby';
-
 
 export const visibleClassNameFromStep = currentStep => step =>
   ((currentStep < step) ? 'hidden' : 'visible');
@@ -12,41 +11,22 @@ export const visibleClassNameUntilStep = currentStep => step =>
   ((currentStep > step) ? 'hidden' : 'visible');
 
 function slideControls(WrappedComponent, previousPage, nextPage, totalSteps = 1) {
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        currentStep: 1
-      };
-      this.handleOnKeyDown = this.onKeyDown.bind(this);
-    }
-
-    render() {
-      return <WrappedComponent {...this.props} currentStep={this.state.currentStep} />;
-    }
-
-    componentDidMount() {
-      document.addEventListener('keydown', this.handleOnKeyDown, false);
-    }
-
-    componentWillUnmount() {
-      document.removeEventListener('keydown', this.handleOnKeyDown, false);
-    }
-
-    onKeyDown(event) {
+  return ({...props}) => {
+    const [currentStep, setCurrentStep] = useState(1);
+    const handleOnKeyDown = (event) => {
       switch (event.key) {
         case 'Left':
         case 'ArrowLeft':
-          if (this.state.currentStep > 1) {
-            this.setState({currentStep: this.state.currentStep - 1});
+          if (currentStep > 1) {
+            setCurrentStep(currentStep - 1);
           } else {
             navigate(previousPage);
           }
           break;
         case 'Right':
         case 'ArrowRight':
-          if (this.state.currentStep < totalSteps) {
-            this.setState({currentStep: this.state.currentStep + 1});
+          if (currentStep < totalSteps) {
+            setCurrentStep(currentStep + 1);
           } else {
             navigate(nextPage);
           }
@@ -57,8 +37,16 @@ function slideControls(WrappedComponent, previousPage, nextPage, totalSteps = 1)
           break;
         default:
       }
-    }
-  };
+    };
+    useEffect(() => {
+      document.addEventListener('keydown', handleOnKeyDown, false);
+      return () => {
+        document.removeEventListener('keydown', handleOnKeyDown, false);
+      };
+    }, [currentStep]);
+
+    return <WrappedComponent {...props} currentStep={currentStep} />;
+  }
 }
 
 export default slideControls;
